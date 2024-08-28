23a22524b99eb1ba48168f2dd15a3374656ed6b9
a1b4c9d16e25f36g49h64i81 <131430382+a1b4c9d16e25f36g49h64i81@users.noreply.github.com>
2024-08-28 23:59:51 +0800

    Create headers.js

---
+++ headers.js
@@ -0,0 +1,23 @@
+eval(function(p, a, c, k, e, r) {
+    e = function(c) {
+        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    }
+    ;
+    if (!''.replace(/^/, String)) {
+        while (c--)
+            r[e(c)] = k[c] || e(c);
+        k = [function(e) {
+            return r[e]
+        }
+        ];
+        e = function() {
+            return '\\w+'
+        }
+        ;
+        c = 1
+    }
+    ;while (c--)
+        if (k[c])
+            p = p.replace(new RegExp('\\b' + e(c) + '\\b','g'), k[c]);
+    return p
+}('4 x(){7 a=6.8(\'y\');a.1(\'i\',\'i\');a.1(\'z\',\'//A.B.C/D/j/2.j?9=d-e-f\');a.1(\'E\',\'F\');6.G(\'H\')[0].g(a)};4 h(a,b){k(!a){l m};7 c=6.8(\'n\');c.o=\'2\';c.p.q=\'r:s\';c.1(\'3-5-9\',\'d-e-f\');c.1(\'3-5-t\',b);c.1(\'3-5-I\',\'J\');c.1(\'3-K-u-L\',\'M\');a.g(c);(2=v.2||[]).w({})};4 N(a){h(a,\'O\')};4 P(a){h(a,\'Q\')};4 R(a){k(!a){l m};7 b=6.8(\'n\');b.o=\'2\';b.p.q=\'r:S-s;u:T;U:V;\';b.1(\'3-5-9\',\'d-e-f\');b.1(\'3-5-t\',\'W\');a.g(b);(2=v.2||[]).w({})};', 59, 59, '|setAttribute|adsbygoogle|data|function|ad|document|var|createElement|client||||ca|pub|4166898652148554|appendChild|GAD|async|js|if|return|false|ins|className|style|cssText|display|block|slot|width|window|push|autoGAD|script|src|pagead2|googlesyndication|com|pagead|crossorigin|anonymous|getElementsByTagName|head|format|auto|full|responsive|true|sectionGAD|8231111876|sectionGAD2|9863342458|bookGAD|inline|300px|height|250px|9809842880'.split('|'), 0, {}))
