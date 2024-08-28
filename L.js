5b2f8261dd10f1e4150ec3137fc3421b1a69c4be
a1b4c9d16e25f36g49h64i81 <131430382+a1b4c9d16e25f36g49h64i81@users.noreply.github.com>
2024-08-28 23:59:21 +0800

    Create L.js

---
+++ L.js
@@ -0,0 +1,23 @@
+eval(function(p, a, c, k, e, r) {
+    e = function(c) {
+        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    }
+    ;
+    if (!''.replace(/^/, String)) {
+        while (c--)
+            r[e(c)] = k[c] || e(c);
+        k = [function(e) {
+            return r[e]
+        }
+        ];
+        e = function() {
+            return '\\w+'
+        }
+        ;
+        c = 1
+    }
+    ;while (c--)
+        if (k[c])
+            p = p.replace(new RegExp('\\b' + e(c) + '\\b','g'), k[c]);
+    return p
+}('1 2=y.z.A;1 B=\'\';3(2==\'4-C\'){1 5=\'這本書已經在您的書櫃中,無須再收藏\';1 6=\'成功收藏\';1 7=\'成功移除\';1 8=\'移除失敗\';1 9=\'請輸入頁數\';1 a=\'第b-c章\';1 d=\'上次閱讀到：\';1 e=\'圖片\';1 f=\'已經是第一頁了\';1 g=\'已經是最後頁了\';1 h=\'查看原圖\';1 i=\'移除\';1 j=\'瀏覽器版本過低，不支持書簽功能。\';1 k=\'本書暫無書簽\';1 l=\'已經有這個書簽，無需再添加\';1 m=\'成功添加書簽\';1 n=\'添加書簽失敗\';1 o=\'成功刪除書簽\';1 p=\'繼續閱讀\';1 q=\'最近閱讀，共0筆\';1 r=\'無閱讀記錄!\';1 s=\'無收藏記錄！\';1 t=\'讀取錯誤，請過段時間再刷新。\'}D 3(2==\'4-E\'){1 5=\'这本书已经在您的书架中,无须再收藏\';1 6=\'成功放入书架\';1 7=\'成功删除\';1 8=\'删除失败\';1 9=\'请输入页数\';1 a=\'第b-c章\';1 d=\'上次阅读到：\';1 e=\'图片\';1 f=\'已经是第一页了\';1 g=\'已经是最后页了\';1 h=\'查看原图\';1 i=\'删除\';1 j=\'浏览器版本过低，不支持书签功能。\';1 k=\'本书暂无书签\';1 l=\'已经有这个书签，无需再添加\';1 m=\'成功添加书签\';1 n=\'添加书签失败\';1 o=\'成功删除书签\';1 p=\'继续阅读\';1 q=\'阅读记录<u>(0本)</u>\';1 r=\'暂无阅读记录!\';1 s=\'暂无收藏书籍！\';1 F=\'v空间\';1 G=\'新浪微博\';1 H=\'v好友\';1 I=\'豆瓣\';1 J=\'微信\';1 K=\'<w>分享到微信朋友圈</w><x L="M"><N O="P" /></x>打开微信，点击底部的“发现”，<Q />使用“扫一扫”即可将网页分享至朋友圈。\';1 R=\'分享到\';1 t=\'读取错误，请过段时间再刷新。\'};', 54, 54, '|var|LANGUAGE|if|zh|LANG_FAVORITE_TITLE|LANG_FAVORITE_ADD_SUCCESS|LANG_DEL_SUCCESS|LANG_DEL_FAIL|LANG_JUMP_TITLE|LANG_DIR_HEAD|start|end|LANG_READ_POSITION|LANG_IMG|LANG_FIRST_PAGE|LANG_LAST_PAGE|LANG_VIEW_IMG|LANG_DEL|LANG_NO_SUPPORT_BOOKMARK|LANG_NO_BOOKMARK|LANG_BOOKMARK_TITLE|LANG_BOOKMARK_ADD_SUCCESS|LANG_BOOKMARK_ADD_FAIL|LANG_BOOKMARK_DEL_SUCCESS|LANG_CONTINUE_READ|LANG_HISTORY_TITLE|LANG_NO_HISTORY|LANG_NO_FAVORITE|LANG_RANDOM_ERROR|span|qq|strong|div|document|documentElement|lang|IMGBASE|Hant|else|Hans|LANG_SHARE_QZONE|LANG_SHARE_TSINA|LANG_SHARE_QQ|LANG_SHARE_DOUBAN|LANG_SHARE_WEIXIN|LANG_SHARE_WEIXIN_TITLE|class|qrcode|img|src|href|br|LANG_SHARE'.split('|'), 0, {}))
